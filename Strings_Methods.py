382b5777aa2e4e9a6a24f012c2cfc41423a4d496
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-06-18 21:34:15 +0530

    Add files via upload

---
+++ Strings_Methods.py
@@ -0,0 +1,121 @@
+'''1.capitalize()'''
+# a="world"
+# print(a.capitalize())  #World
+# b="cOMPUTER"
+# print(b.capitalize())  #Computer
+
+'''2.casefold()'''
+# a='PYTHON PROGRAM'
+# print(a.casefold())  #python program
+
+'''3.center()'''
+# a='movie'
+# print(a.center(9,'0'))  #00movie00
+# b='serial'
+# print(b.center(15,'3'))   #33333serial3333
+
+'''4.count()'''
+# a="73637777777836"
+# print(a.count('7'))  #8
+# a="hhhddukskkkk"
+# print(a.count('k'))  #5
+
+'''5.encode()'''
+# a="awesomeprogram"
+# print(a.encode())  #b'awesomeprogram'
+# a='55667'
+# print(a.encode())  #b'55667'
+
+'''6.endswith()'''
+# n="abc5"
+# print(n.endswith('5'))  #True
+# m="hey9"
+# print(m.endswith('9'))  #True
+# print(m.endswith('5'))  #False 
+
+'''7.find()'''
+# x='87123456'
+# print(x.find('4'))  #5
+# print(x.find('7'))  #1
+# print(x.find('6'))  #7
+
+'''8.index()'''
+# y="im writing python program"
+# print(y.index('python')) #11
+
+'''9.isalnum()'''
+# z="1a2b3c4d"
+# print(z.isalnum())  #True
+
+'''10.isalpha()'''
+# n="abcdefg"
+# print(n.isalpha())  #True
+# m="d576dd98"
+# print(m.isalpha())  #False
+
+'''11.isdecimal()'''
+# a="00032"
+# print(a.isdecimal())  #True
+
+'''12.isdigit()'''
+# b="37353902"
+# print(b.isdigit())  #True
+# c="swbhjs78y"
+# print(c.isdigit())  #False
+
+'''13.isidentifier()'''
+# a="hello"
+# print(a.isidentifier())  #True
+# b="3hey"
+# print(b.isidentifier())  #False
+# c="_fine33"
+# print(c.isidentifier())  #True
+
+'''14.islower()'''
+# x="clock"
+# print(x.islower())  #True
+# y="DIRECTIOn"
+# print(y.islower())  #False
+
+'''15.isnumeric()'''
+# p="2456256256"
+# print(p.isnumeric())  #True
+
+'''16.isprintable()'''
+# q="im doing well"
+# print(q.isprintable())  #True
+
+'''17.isupper()'''
+# z="SINDHU"
+# print(z.isupper())  #True
+# n="sindhu"
+# print(n.isupper())  #False
+
+'''18.lower()'''
+# a="WELCOME"
+# print(a.lower())  #welcome
+
+'''19.upper()'''
+# b="hyderabad"
+# print(b.upper())  #HYDERABAD
+
+'''20.title()'''
+# c="how are you doing"
+# print(c.title())  #How Are You Doing
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
